b74155eede8b0cbfb35bc7d682cb7435e10f816f
reinvald <reinvald@seas.upenn.edu>
2020-03-28 14:26:05 -0400

    Updates

---
+++ precache-manifest.203b247fc29be843959bb1b7fbc1de9e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbd4073feaea603609f85fe16ee86ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0fcae4eea4436d2fdf7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "de80960bf429702d7958",
+    "url": "/static/css/main.a38d1289.chunk.css"
+  },
+  {
+    "revision": "e0fcae4eea4436d2fdf7",
+    "url": "/static/js/2.f773fbdc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f773fbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de80960bf429702d7958",
+    "url": "/static/js/main.b2afe959.chunk.js"
+  },
+  {
+    "revision": "87c7a3692d491a14676e",
+    "url": "/static/js/runtime-main.40f9e888.js"
+  },
+  {
+    "revision": "a29a418536071403a914a573466c3926",
+    "url": "/static/media/2-line-whitetext-colorshield.a29a4185.png"
+  },
+  {
+    "revision": "5d14ba24113efb419d183a19b7052d8f",
+    "url": "/static/media/aavo_greyscale.5d14ba24.png"
+  },
+  {
+    "revision": "9b92bd5e088168afd4599f9ede1b2951",
+    "url": "/static/media/kisspng-the-beatles-magical-mystery-tour-abbey-road-yellow-walrus-5acc006e8a2674.5985732215233188945659.9b92bd5e.png"
+  },
+  {
+    "revision": "a5386c065a0a4cd0c8a4ecb036ef0575",
+    "url": "/static/media/logo.a5386c06.png"
+  },
+  {
+    "revision": "5152b5a652b84e8572c5d527aa590e82",
+    "url": "/static/media/monaco.5152b5a6.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reinvald-react/precache-manifest.3833c32eb5d280e9eb0cf7d272b984a9.js"
+  "/precache-manifest.203b247fc29be843959bb1b7fbc1de9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reinvald-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
