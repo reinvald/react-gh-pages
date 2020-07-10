f76c33476ae10bbcb7d984a69904b1bf233bf98d
reinvald <reinvald@seas.upenn.edu>
2020-07-10 13:59:22 -0400

    testing routes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,9 +10,9 @@ class Header extends Component {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="about">about</Nav.Link>
-            <Nav.Link href="technical">technical</Nav.Link>
-            <Nav.Link href="fun">fun + misc</Nav.Link>
+            <Nav.Link href="/about">about</Nav.Link>
+            <Nav.Link href="/technical">technical</Nav.Link>
+            <Nav.Link href="/fun">fun + misc</Nav.Link>
           </Nav>
           <Nav className="ml-auto">
             <div class="icons">
